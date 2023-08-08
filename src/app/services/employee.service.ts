952852a392b976a30849a2bfd6ffba18824a623a
M-Sundar <meenatchisundaram.t@kaplan.com>
2023-08-08 14:51:22 +0530

    changed api endpoint link

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -16,23 +16,23 @@ export class EmployeeService {
   constructor(private http: HttpClient, private snackbar: MatSnackBar, private dialog: MatDialog) { }
 
   addEmployee(data: any): Observable<any> {
-    return this.http.post('https://crudcrud.com/api/e95c89805aab41af94631cfb14bfdaf5/resource/', data);
+    return this.http.post('https://crudcrud.com/api/1f131474efcc4cfeb586367b86db7cc8/resource/', data);
    }
 
    getEmployee(): Observable<any>{
-    return this.http.get('https://crudcrud.com/api/e95c89805aab41af94631cfb14bfdaf5/resource/');
+    return this.http.get('https://crudcrud.com/api/1f131474efcc4cfeb586367b86db7cc8/resource/');
    }
 
    delete(id: any): Observable<any>{
-    return this.http.delete(`https://crudcrud.com/api/e95c89805aab41af94631cfb14bfdaf5/resource/${id}`);
+    return this.http.delete(`https://crudcrud.com/api/1f131474efcc4cfeb586367b86db7cc8/resource/${id}`);
    }
 
    update(data: UserProfile, id: number): Observable<any>{
-    return this.http.put<UserProfile>(`https://crudcrud.com/api/e95c89805aab41af94631cfb14bfdaf5/resource/${id}`, data);
+    return this.http.put<UserProfile>(`https://crudcrud.com/api/1f131474efcc4cfeb586367b86db7cc8/resource/${id}`, data);
    }
 
    getUserId(id: any){
-    return this.http.get<UserProfile>(`https://crudcrud.com/api/e95c89805aab41af94631cfb14bfdaf5/resource/${id}`);
+    return this.http.get<UserProfile>(`https://crudcrud.com/api/1f131474efcc4cfeb586367b86db7cc8/resource/${id}`);
    }
 
    openSnackBar(msg: string, action: string = 'Ok'){
